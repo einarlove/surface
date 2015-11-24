5c3da676b4b021a04c6cf3de54b694b0915e5808
Einar Löve <einar.love@gmail.com>
2015-11-24 16:41:07 +0100

    add experation support

--- src/selectors/prioritizeByRelevance/filterIrrelevant.js
+++ src/selectors/prioritizeByRelevance/filterIrrelevant.js
@@ -1,22 +1,34 @@
 import isUndefined from 'lodash/lang/isUndefined'
+import moment from 'moment'
 
 const bothHasPropertyAndMismatch = (left, right) => {
   return !isUndefined(left) && !isUndefined(right) && left !== right
 }
 
 export default state => ({meta}) => {
-  const flight = state.flights.current
+  if (state.flights.current) {
+    const flight = state.flights.current
+    if (bothHasPropertyAndMismatch(meta.direction, flight.direction)) {
+      return false
+    }
 
-  if (!flight) {
-    return true
+    if (bothHasPropertyAndMismatch(meta.international, flight.international)) {
+      return false
+    }
   }
 
-  if (bothHasPropertyAndMismatch(meta.direction, flight.direction)) {
-    return false
-  }
 
-  if (bothHasPropertyAndMismatch(meta.international, flight.international)) {
-    return false
+  if (meta.expiration) {
+    const now = moment()
+    const {from, to} = meta.expiration
+
+    if (from && now.isBefore(from)) {
+      return false
+    }
+
+    if (to && now.isAfter(to)) {
+      return false
+    }
   }
 
   return true
